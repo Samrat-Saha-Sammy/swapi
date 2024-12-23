12259e1c11b3bad7f6f74db386db784437e3b8b5
Samrat Saha <aaniruddhaaa@gmail.com>
2024-12-23 19:16:57 +0530

    Added Lazy loading of pages

--- src/pages/root/App.tsx
+++ src/pages/root/App.tsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import "./App.css";
-import ListPage from "../list-page";
 import { Route, Routes } from "react-router-dom";
-import NotFoundPage from "../404-page";
-import DetailsPage from "../details-page";
-import FavouritesPage from "../favourites-page";
 import MainLayout from "../layouts/main-layout";
+import NotFoundPage from "../404-page";
+// Lazy-loaded Pages
+const ListPage = lazy(() => import("../list-page"));
+const DetailsPage = lazy(() => import("../details-page"));
+const FavouritesPage = lazy(() => import("..//favourites-page"));
 
 const App: React.FC = () => {
 	return (
-		<Routes>
-			<Route path="/" element={<MainLayout />}>
-				<Route index element={<ListPage />} />
-				<Route path="/details/:id" element={<DetailsPage />} />
-				<Route path="/liked" element={<FavouritesPage />} />
-			</Route>
-			<Route path="*" element={<NotFoundPage />} /> {/* Catch-all route */}
-		</Routes>
+		<Suspense fallback={<div>Loading...</div>}>
+			<Routes>
+				<Route path="/" element={<MainLayout />}>
+					<Route index element={<ListPage />} />
+					<Route path="/details/:id" element={<DetailsPage />} />
+					<Route path="/liked" element={<FavouritesPage />} />
+				</Route>
+				<Route path="*" element={<NotFoundPage />} /> {/* Catch-all route */}
+			</Routes>
+		</Suspense>
 	);
 };
 
