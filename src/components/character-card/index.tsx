917d33614db9dd87b341c77c7b02a793e2b9b1cf
Samrat Saha <aaniruddhaaa@gmail.com>
2024-12-24 14:03:33 +0530

    Added accessibility for the file

--- src/components/character-card/index.tsx
+++ src/components/character-card/index.tsx
@@ -48,17 +48,24 @@ const CharacterCard: React.FC<{
 	}, [cid]);
 
 	return (
-		<li className="flex items-center justify-between rounded-lg border border-gray-200 bg-gray-50 p-4 shadow-sm">
+		<li
+			className="flex items-center justify-between rounded-lg border border-gray-200 bg-gray-50 p-4 shadow-sm"
+			role="listitem"
+			aria-labelledby={`character-${cid}`}
+		>
 			<div>
-				<h2 className="text-lg font-semibold ">{character?.name}</h2>
+				<h2 id={`character-${cid}`} className="text-lg font-semibold ">
+					{character?.name}
+				</h2>
 				<p className="text-sm text-gray-600 flex space-x-2">
-					<span>Gender: {`${character?.gender}`}</span>
-					<span>Home Planet: {`${planet?.name}`}</span>
+					<span>Gender: {character?.gender}</span>
+					<span>Home Planet: {planet?.name}</span>
 				</p>
 			</div>
 			<button
 				className="px-6 py-2 min-w-[120px] text-center text-violet-600 border border-violet-600 rounded hover:bg-violet-600 hover:text-white active:bg-indigo-500 focus:outline-none focus:ring"
 				onClick={() => handleDetailsClick(cid)}
+				aria-label={`View details for ${character?.name}`}
 			>
 				{btnText}
 			</button>
